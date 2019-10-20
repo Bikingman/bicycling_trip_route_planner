638c0c05b0ee69f882f0e9aeeacafe607339251a
Bikingman <daniel.patterson87@gmail.com>
2019-10-20 14:55:09 -0400

    zomm updates

--- docs/scripts/01_main.js
+++ docs/scripts/01_main.js
@@ -16,7 +16,7 @@ var CartoDB_DarkMatterOnlyLabels = L.tileLayer('https://{s}.basemaps.cartocdn.co
 var Stamen_Terrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
-	minZoom: 0,
+	minZoom: 2,
 	maxZoom: 18,
 	ext: 'png'
 });
@@ -24,26 +24,25 @@ $('mapid').height(window.innerHeight)
 
 var map = L.map('mapid', {
 	renderer: L.canvas(),
-  center: [38.893908, -77.036541],
-  zoom: 13,
-  layers: [CartoDB_DarkMatterNoLabels, CartoDB_DarkMatterOnlyLabels],
-  doubleClickZoom: false,
+  center: [39.002434, -77.181233],
+  zoom: 11,
+  layers: [Stamen_Terrain],
+  doubleClickZoom: true,
   boxZoom: false,
   zoomControl: true,
-  minZoom: 12
+  minZoom: 0
 });
 
 
 L.Routing.control({
         waypoints: [
-          L.latLng(38.893908, -77.0155),
+          L.latLng(39.138392, -77.406748),
           L.latLng(38.8940, -77.036541)
         ],
         routeWhileDragging: true,
-        geocoder: L.Control.Geocoder.nominatim()
     })
     .on('routeselected', function(e) {
         var route = e.route;
-        console.log('Showing route between waypoints:\n' + JSON.stringify(route.coordinates, null, 2));
+        console.log('Showing route between waypoints:\n' + JSON.stringify(route.coordinates, null, 4));
     })
     .addTo(map);
